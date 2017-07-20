ac78fb8d48d6560d526a2d13d013c1d03017f525
yesdoing <fkrmtleo12@gmail.com>
2017-07-20 15:50:07 +0900

    GET /user/:id - id를 반환하는 test코드를 작성

--- index.spec.js
+++ index.spec.js
@@ -33,4 +33,17 @@ describe('GET /users는', () => {
                 .end(done);
         })
     })
+});
+
+describe('GET /users는', () => {
+     describe('성공 시', ()=> {
+         it('id가 1인 유저 객체를 반환한다.', (done) => {
+            request(app)
+                .get('/users/1')
+                .end((err, res) => {
+                    res.body.should.have.property('id', 1);
+                    done();
+                })
+         })
+     })
 })
